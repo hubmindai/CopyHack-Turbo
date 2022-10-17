83a5d03ff5ed99b49c095c5c9d101e23aba2d6cc
hubmindllc <103978758+hubmindllc@users.noreply.github.com>
2022-10-16 23:20:15 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -14,16 +14,11 @@ module.exports = {
     client: 'pg',
     version: '13',
     connection: {
-      host: process.env.DB_DEV_HOST,
-      user: process.env.DB_DEV_USER,
-      password: process.env.DB_DEV_PASSWORD,
-      database: process.env.DB_DEV_DATABASE,
-      ssl: {
-    rejectUnauthorized: true,
-    // ca: fs.readFileSync(
-    //     `${process.cwd()}/cert/ca-certificate.crt`.toString()
-    // ),
-    ca: process.env.CA_CERT,
+      host: 'db-postgresql-nyc1-do-user-10528061-0.b.db.ondigitalocean.com',
+      port: '25060',
+      user: 'doadmin',
+      password: 'AVNS_hFsDkFF_E0fLQ-Rfx3-',
+      database: 'dbdev',
 }
     },
     pool: {
@@ -43,16 +38,10 @@ module.exports = {
     client: 'pg',
     version: '13',
     connection: {
-      host: process.env.DB_PROD_HOST,
-      user: process.env.DB_PROD_USER,
-      password: process.env.DB_PROD_PASSWORD,
-      database: process.env.DB_PROD_DATABASE,
-      ssl: {
-    rejectUnauthorized: true,
-    // ca: fs.readFileSync(
-    //     `${process.cwd()}/cert/ca-certificate.crt`.toString()
-    // ),
-    ca: process.env.CA_CERT,
+      host: 'db-postgresql-nyc1-do-user-10528061-0.b.db.ondigitalocean.com',
+      user: 'doadmin',
+      password: 'AVNS_hFsDkFF_E0fLQ-Rfx3-',
+      database: 'dbprod',
 }
     },
     pool: {
