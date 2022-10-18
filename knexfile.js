d6be8a73aee00379d70befadde36d4a881e1dec2
hubmindllc <103978758+hubmindllc@users.noreply.github.com>
2022-10-18 01:43:15 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -14,11 +14,10 @@ module.exports = {
     client: 'pg',
     version: '13',
     connection: {
-      host: 'db-postgresql-nyc1-do-user-10528061-0.b.db.ondigitalocean.com',
-      port: '25060',
-      user: 'doadmin',
-      password: 'AVNS_hFsDkFF_E0fLQ-Rfx3-',
-      database: 'dbdev'
+      host: process.env.DB_DEV_HOST,
+      user: process.env.DB_DEV_USER,
+      password: process.env.DB_DEV_PASSWORD,
+      database: process.env.DB_DEV_DATABASE
     },
     pool: {
       min: 2,
@@ -37,11 +36,10 @@ module.exports = {
     client: 'pg',
     version: '13',
     connection: {
-      host: 'db-postgresql-nyc1-do-user-10528061-0.b.db.ondigitalocean.com',
-      port: '25060',
-      user: 'doadmin',
-      password: 'AVNS_hFsDkFF_E0fLQ-Rfx3-',
-      database: 'dbprod'
+      host: process.env.DB_PROD_HOST,
+      user: process.env.DB_PROD_USER,
+      password: process.env.DB_PROD_PASSWORD,
+      database: process.env.DB_PROD_DATABASE
     },
     pool: {
       min: 2,
